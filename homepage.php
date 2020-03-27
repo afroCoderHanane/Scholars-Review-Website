1ccc8f3a182035ac57a109113665ee0e3a70aeba
abdoulhanane.gbadamassi@stu.bmcc.cuny.edu <abdoulhanane.gbadamassi@stu.bmcc.cuny.edu>
2020-03-26 20:39:58 -0400

    commit

--- homepage.php
+++ homepage.php
@@ -227,7 +227,7 @@ session_start();
 
 <!-- old code -->
 
- <div id = "main-wrapper">
+ <!--div id = "main-wrapper">
      <center>
        <h2> Home Page </h2>
        <h3> Welcome <?php echo $_SESSION['username']?></h3>
@@ -245,7 +245,7 @@ if(isset($_POST['logout']))
    session_destroy();
    header('location:login.php');
  }
-?>
+?>-->
 </div>
 </body>
 </html>
