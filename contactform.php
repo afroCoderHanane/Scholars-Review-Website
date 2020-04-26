0d86b0c7f2c99f53c371b6d10f7cf2bcaf3dda5e
MaltoMight <48389718+MaltoMight@users.noreply.github.com>
2020-04-26 14:44:21 -0400

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -5,7 +5,7 @@ $email = $_POST['email'];
 $title = $_POST['titl'];
 $message = $_POST['message'];
 $formcontent=" From: $name \n Message: $message";
-$recipient = "yipguoli123@gmail.com";
+$recipient = "youremail@here.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
