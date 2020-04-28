4e56b09eebdd629964b0e4ab9fd674519b6b83c2
abdoulhanane.gbadamassi@stu.bmcc.cuny.edu <abdoulhanane.gbadamassi@stu.bmcc.cuny.edu>
2020-04-28 17:24:02 -0400

    update

--- template/footpage.php
+++ template/footpage.php
@@ -27,18 +27,18 @@
 
           <div class="footer_third">
               <h1>More Learning Resources</h1>
-              <a href="edx.org">Edx</a>
-              <a href="coursera.org"><i class="fas fa-infinity"> Coursera</i></a>
+              <a href="https://www.edx.org/">Edx</a>
+              <a href="https://www.coursera.org"><i class="fas fa-infinity"> Coursera</i></a>
               <a href="http://tutorial.math.lamar.edu/">Math Online</a>
 
           </div>  
 
           <div class="footer_third">
               <h1>Follow Us</h1>
-              <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-              <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-              <li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+              <li><a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a></li>
+              <li><a href="https://web.whatsapp.com/"><i class="fab fa-whatsapp"></i></a></li>
           <span>
               
               Borough of Manhattan Community College<br>
