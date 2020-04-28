24fc4705e21dd69b1e9a54a18abad16ff2caaa60
MaltoMight <48389718+MaltoMight@users.noreply.github.com>
2020-04-27 20:46:35 -0400

    Create footpage.php

---
+++ template/footpage.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Stylesheets -->
+  <link rel="stylesheet" type="text/css" href="../CSS/footer.css">
+  <script src="https://kit.fontawesome.com/f0e157d390.js" crossorigin="anonymous"></script>
+
+  <!-- Icons -->
+  <title></title>
+</head>
+<body>
+  <div class="footer">
+      <div class="inner_footer">
+          <div class="logo_container">
+             <img><i class="fas fa-brain"></i></img>
+             <h2> Scholars Review </h2>
+          </div>
+
+          <div class="footer_third">
+              <h1>Need Help?</h1>
+              <a href="#">Terms &amp; Conditions</a>
+              <a href="#">Privacy Policy</a>
+          </div>
+
+          <div class="footer_third">
+              <h1>More</h1>
+              <a href="#">Tag1</a>
+              <a href="#">Tag2</a>
+              <a href="#">Tag3</a>
+
+          </div>  
+
+          <div class="footer_third">
+              <h1>Follow Us</h1>
+              <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+              <li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
+          <span>
+              
+              Borough of Manhattan Community College<br>
+              199 Chambers St, <br>
+              New York, NY 10007 <br>
+              USA 
+          </span>
+        </div>
+
+    </div>
+  </div>
+</body>
+</html>
