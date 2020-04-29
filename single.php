24741ae1cf6d44e3d7841808d606ee89a458fe4d
abdoulhanane.gbadamassi@stu.bmcc.cuny.edu <abdoulhanane.gbadamassi@stu.bmcc.cuny.edu>
2020-04-29 14:09:41 -0400

    commit

--- single.php
+++ single.php
@@ -18,18 +18,31 @@ session_start();
     <!--Content-->
     <div class="content clearfix">
 
-       <div class="main-content"> <!--Main content-->
+       <div class="main-content single"> <!--Main content-->
       
-       <h2 class="post-title">This is the title</h2>
-       <p>
-      
-i porta lorem mollis aliquam ut porttitor leo a. Nunc sed velit dignissim sodales ut. Odio pellentesque diam volutpat commodo sed egestas egestas. Ut tristique et egestas quis. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Nulla pellentesque dignissim enim sit amet. Dignissim sodales ut eu sem integer vitae justo. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Duis ut diam quam nulla porttitor massa id. Tellus in hac habitasse platea dictumst. Donec ac odio tempor orci dapibus ultrices in iaculis. Eu lobortis elementum nibh tellus molestie nunc non. Mi eget mauris pharetra et ultrices neque ornare aenean euismod. Nunc mattis enim ut tellus. Sem fringilla ut morbi tincidunt augue interdum. Turpis tincidunt id aliquet risus. Viverra orci sagittis eu volutpat odio facilisis mauris sit.
-       </p>
+       <h1 class="post-title">This is the title</h1>
+       
+       <div class="post-content">
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur commodi deleniti ea blanditiis mollitia laudantium?
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non animi cupiditate ullam, minima perspiciatis natus corrupti assumenda rem repudiandae. Voluptate non excepturi similique earum impedit iusto eaque eum nam velit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus sapiente unde molestias perferendis, a eos amet dolore tempore magnam nemo aspernatur odit accusantium quae accusamus tenetur cumque ipsam asperiores. Minus!
+          Lorem ipsum dolor sit amet.
+         </p>
+         <p>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa sapien faucibus et molestie ac feugiat sed lectus. Semper quis lectus nulla at volutpat diam. Amet mauris commodo quis imperdiet massa tincidunt. Elementum facilisis leo vel fringilla est ullamcorper eget nulla. Amet mauris commodo quis imperdiet massa tincidunt. Velit aliquet sagittis id consectetur purus. Massa tincidunt nunc pulvinar sapien et ligula. Cursus metus aliquam eleifend mi in nulla posuere. Elementum sagittis vitae et leo duis. Vitae elementum curabitur vitae nunc sed velit dignissim sodales ut. Quis commodo odio aenean sed adipiscing. Et netus et malesuada fames ac turpis egestas integer. Porta lorem mollis aliquam ut porttitor leo a. Mauris augue neque gravida in. Cras pulvinar mattis nunc sed blandit libero. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula.
+
+At elementum eu facilisis sed. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis. Egestas diam in arcu cursus euismod quis. Amet purus gravida quis blandit turpis. Eget velit aliquet sagittis id. Nunc sed blandit libero volutpat sed cras ornare. Libero justo laoreet sit amet cursus sit amet dictum. Nibh tellus molestie nunc non. Pellentesque nec nam aliquam sem et tortor consequat. Massa id neque aliquam vestibulum. Risus nullam eget felis eget nunc lobortis mattis aliquam. Aliquet nibh praesent tristique magna. Ipsum dolor sit amet consectetur. Purus semper eget duis at tellus at urna condimentum. Nunc sed augue lacus viverra vitae congue.
+
+Nunc sed augue lacus viverra vitae congue eu. Arcu ac tortor dignissim convallis aenean et tortor. Eget duis at tellus at urna condimentum mattis pellentesque id. Gravida quis blandit turpis cursus in hac habitasse platea. Ligula ullamcorper malesuada proin libero nunc consequat interdum. Tortor at auctor urna nunc. Nunc non blandit massa enim nec dui nunc. Et ultrices neque ornare aenean euismod elementum nisi quis. Malesuada fames ac turpis egestas sed tempus. Nisi est sit amet facilisis. Hac habitasse platea dictumst quisque sagittis. Mi quis hendrerit dolor magna eget.
+
+Consequat semper viverra nam libero justo. Fames ac turpis egestas maecenas pharetra. Magna sit amet purus gravida quis blandit turpis cursus in. Sem et tortor consequat id. Ultrices in iaculis nunc sed augue lacus viverra. Aenean pharetra magna ac placerat vestibulum lectus mauris. Placerat duis ultricies lacus sed turpis tincidunt id aliquet risus. Habitasse platea dictumst quisque sagittis purus sit amet. Vitae suscipit tellus mauris a diam maecenas sed enim. Congue mauris rhoncus aenean vel elit. Scelerisque eu ultrices vitae auctor eu. Lorem ipsum dolor sit amet. Id volutpat lacus laoreet non curabitur. Et magnis dis parturient montes nascetur ridiculus mus.
+
+Nisl nunc mi ipsum faucibus vitae. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Et malesuada fames ac turpis egestas maecenas pharetra convallis. Purus semper eget duis at tellus at urna. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. Augue mauris augue neque gravida in. In fermentum et sollicitudin ac orci. Et magnis dis parturient montes nascetur ridiculus. Purus sit amet luctus venenatis lectus magna fringilla urna. Non blandit massa enim nec dui nunc mattis. Risus feugiat in ante metus dictum. Nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet. Tristique nulla aliquet enim tortor at. Integer eget aliquet nibh praesent tristique magna.
+         </p>
+       </div>
 
       </div><!-- end Main content-->
       <!--Sidebar-->
-     <div class="sidebar">
-
          <div class="section topics">
           <h2 class="section-title">Topics</h2>
           <ul>
